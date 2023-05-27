dbb9e637fb0fc8e4e483bcaf14764b797cee52d6
COYO <dayeon.yoon@gmail.com>
2023-05-28 00:51:02 +0900

    STYLE: Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import loadable from '@loadable/component';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
+
+const MainLayout = loadable(() => import('./layouts/MainLayout'));
 const MainPage = loadable(() => import('./pages/MainPage'));
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainPage />} path={'*'} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route element={<MainPage />} path={'*'} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+const theme = {
+  layout: {
+    headerHeight: '80px',
+    sidebarWidth: '200px',
+  },
+  colors: {},
+  button: {
+    main: {
+      default: {},
+      hover: {},
+      disabled: {},
+    },
+  },
+};
+
+export default theme;
